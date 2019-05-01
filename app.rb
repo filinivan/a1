7a4340bb4b9334b29304d3d099bc4c73c6637b82
Ivan Filin <filin87@gmail.com>
2019-05-02 01:14:03 +0300

    1

--- app.rb
+++ app.rb
@@ -7,9 +7,26 @@ require 'sinatra/activerecord'
 #Подключение базы данных
 set :database, "sqlite3:a1.db"
 
-class Word < ActiveRecord::Base 
+class Words < ActiveRecord::Base
 end
 
 get '/' do
+#	@words = Word.find(params[:id])
 	erb :index
+end
+
+get '/add' do
+	erb :add
+end
+
+post '/add' do
+  @w = Words.new params[:word]
+
+  if @w.save
+    erb "Вы записаны"
+  else
+    @error = @w.errors.full_messages.first
+    erb :add
+  end
+
 end
